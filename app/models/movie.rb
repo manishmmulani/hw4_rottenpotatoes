d036a697464e56496c06c0855afd624313382b5f
Manish Mulani <mulanimonty@gmail.com>
2012-08-16 12:15:14 -0700

    Added spec code for movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,8 @@ class Movie < ActiveRecord::Base
   def self.all_ratings
     %w(G PG PG-13 NC-17 R)
   end
+
+  def get_movies_by_same_director
+    Movie.find_all_by_director(director)
+  end
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,2 +1,13 @@
 require 'spec_helper'
 
+describe Movie do
+
+  describe 'Get Movies by same director' do
+    it 'should call Movies find_all_by_director method' do
+      movie = Movie.new({:title => "hello", :director => "bond"})
+      Movie.should_receive(:find_all_by_director).with("bond")
+      movie.get_movies_by_same_director
+    end
+  end
+
+end

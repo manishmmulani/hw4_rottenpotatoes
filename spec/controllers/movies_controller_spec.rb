8c52cd6e513d4be303898e5335c05bc3798900b6
Manish Mulani <mulanimonty@gmail.com>
2012-08-16 14:59:00 -0700

    kkjsf

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -38,4 +38,69 @@ describe MoviesController do
     end
 
   end
+
+  describe 'New method' do
+    it 'should be successful' do
+      get :new
+      response.should be_success
+    end
+  end
+
+  describe 'Edit method' do
+    it 'should call edit method' do
+      Movie.should_receive(:find)
+      get :edit, :id => 1
+    end
+  end
+
+  describe 'Create method' do
+    it 'should call create method' do
+      Movie.should_receive(:create!).and_return(@fake_movie)
+      post :create
+    end
+  end
+
+  describe 'Update method' do
+    it 'should call update attributes on a movie' do
+      Movie.stub(:find).and_return(@fake_movie)
+      @fake_movie.should_receive(:update_attributes!)
+      put :update, :id => 1
+    end
+  end
+
+  describe 'Destroy method' do
+    it 'should call destroy' do
+      Movie.stub(:find).and_return(@fake_movie)
+      @fake_movie.should_receive(:destroy)
+      post :destroy, :id => 1
+    end
+  end
+
+  describe 'Index action' do
+      it 'should set the ordering to title and session not defined' do
+        Movie.stub(:find_all_by_rating)
+        get :index, :sort => :title
+      end
+
+      it 'should skip first block' do
+        Movie.stub(:find_all_by_rating)
+        session.stub(:[]).with("flash").and_return double(:sweep => true, :update => true, :[]= => [], :keep => true)
+        session.stub(:[]).with(:sort).and_return(:title)
+        session.stub(:[]).with(:ratings).and_return({"G"=>"1"})
+        get :index, :sort => :title, :ratings => nil
+      end
+
+      it 'should set the ordering to title and session defined' do
+        Movie.stub(:find_all_by_rating)
+        session.stub(:[]).with("flash").and_return double(:sweep => true, :update => true, :[]= => [], :keep => true)
+        session.stub(:[]).with(:sort).and_return(:title)
+        session.stub(:[]).with(:ratings).and_return(nil)
+        get :index, :sort => :title, :ratings => nil
+      end
+
+      it 'should set the ordering to release_date' do
+        Movie.stub(:find_all_by_rating)
+        get :index, :sort => :release_date
+      end
+  end
 end
